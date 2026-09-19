ae6fe0ad79c9b36b5f9b1b41d8d1a39a11bbcf4d
Kaelan <megakg@outlook.com>
2026-09-19 22:05:16 +1000

    Add decoder for Questions

--- ReportPlugin/wp-content/plugins/SurveyReportGeneratorPlugin/surveyReportGeneratorPlugin.php
+++ ReportPlugin/wp-content/plugins/SurveyReportGeneratorPlugin/surveyReportGeneratorPlugin.php
@@ -76,6 +76,107 @@ function get_gravity_forms_list() {
 }
 
 
+/**
+ * Get a form's questions and answer mappings.
+ *
+ * @param int $form_id
+ * @return array
+ */
+function get_form_questi*n_map( $form_id ) {
 
+    $url    =*site_url( '/wp-json/gf/v2/forms/' * $form_id );
+    $method = 'GET';
+*    $oauth = new OAuth_Request(
+  *     $url,
+        GF_CONSUMER_KEY*
+        GF_CONSUMER_SECRET,
+     *  $method
+    );
+
+    $response = *p_remote_request(
+        $oauth->*et_url(),
+        [
+            'method' => $method,
+        ]
+    );*
+    if (
+        is_wp_error( $re*ponse ) ||
+        wp_remote_retri*ve_response_code( $response ) !== *00
+    ) {
+        return [];
+    *
+
+    $form = json_decode(
+       *wp_remote_retrieve_body( $response*),
+        true
+    );
+
+    $questions = [];
+
+    foreach ( $form['fields'] as $field ) {
+
+        // Ignore layout/content fields.
+        if ( in_array(
+            $field['type'],
+            [ 'html', 'page', 'section', 'captcha' ],
+            true
+        ) ) {
+            continue;
+        }
+
+        $question = [
+            'id'      => $field['id'],
+            'type'    => $field['type'],
+            'label'   => $field['label'],
+            'inputs'  => [],
+            'choices' => [],
+        ];
+
+        /*
+         * Input mappings
+         */
+        if ( ! empty( $field['inputs'] ) ) {
+
+            foreach ( $field['inputs'] as $input ) {
+
+                $question['inputs'][] = [
+                    'id'    => $input['id'] ?? null,
+                    'name'  => $input['name'] ?? null,
+                    'label' => $input['label'] ?? null,
+                ];
+            }
+        }
+
+        /*
+         * Choice mappings
+         */
+        if ( ! empty( $field['choices'] ) && is_array( $field['choices'] ) ) {
+
+            foreach ( $field['choices'] as $choice ) {
+
+                $value = $choice['value'] ?? $choice['text'];
+
+                $question['choices'][ $value ] = $choice['text'];
+            }
+        }
+
+        /*
+         * Likert survey row mappings.
+         */
+        if ( ! empty( $field['gsurveyLikertRows'] ) ) {
+
+            $question['rows'] = [];
+
+            foreach ( $field['gsurveyLikertRows'] as $row ) {
+
+                $question['rows'][ $row['value'] ] = $row['text'];
+            }
+        }
+
+        $questions[ $field['id'] ] = $question;
+    }
+
+    return $questions;
+}
 
 ?>
